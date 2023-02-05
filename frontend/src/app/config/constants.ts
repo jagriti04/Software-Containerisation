ea6966f870bf18ef63c2740988571325510108d4
Sushmita <sushmitathakur1993@gmail.com>
2023-02-05 22:38:58 +0100

    Final README

--- frontend/src/app/config/constants.ts
+++ frontend/src/app/config/constants.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'; 
 @Injectable() 
 export class Constants {
-public static API_ENDPOINT: string = 'https://restapi-b553tio6uq-ez.a.run.app/';
+public static API_ENDPOINT: string = 'http://rest-api:2000/';
 }
